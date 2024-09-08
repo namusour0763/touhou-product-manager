0c1a39ffac65408c85a985b07c30d33998bba679
namusour0763 <176794157+namusour0763@users.noreply.github.com>
2024-09-09 00:06:05 +0900

    initial commit

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,9 +1,12 @@
 import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
 
-export const products = pgTable("product", {
+export const product = pgTable("product", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
   character: varchar("character", { length: 255 }).notNull(),
   category: varchar("category", { length: 255 }).notNull(),
   price: integer("price").notNull(),
 });
+
+export type Product = typeof product.$inferSelect;
+export type NewProduct = typeof product.$inferInsert;
